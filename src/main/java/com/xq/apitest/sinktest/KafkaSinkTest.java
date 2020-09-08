4d7018d0b62cc20fec875217b4d397003170db50
dike <82329839@QQ.com>
2020-09-08 18:50:04 +0800

    add

--- src/main/java/com/xq/apitest/sinktest/KafkaSinkTest.java
+++ src/main/java/com/xq/apitest/sinktest/KafkaSinkTest.java
@@ -2,8 +2,10 @@ package com.xq.apitest.sinktest;
 
 import com.xq.apitest.pojo.SensorReading;
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.serialization.DeserializationSchema;
 import org.apache.flink.api.common.serialization.SimpleStringEncoder;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.configuration.ConfigConstants;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.core.fs.Path;
@@ -14,6 +16,8 @@ import org.apache.flink.streaming.api.functions.sink.filesystem.StreamingFileSin
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer011;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.Properties;
 
 public class KafkaSinkTest {
@@ -32,22 +36,43 @@ public class KafkaSinkTest {
         properties.setProperty("auto.offset.reset", "latest");
 //        properties.setProperty("auto.offset.reset", "earliest");
 // 从文件读取数据
-        DataStreamSource<String> inputStream = env.readTextFile("D:\\code\\FlinkTutorial_1.10\\src\\main\\resources\\sensor.txt");
+//        DataStreamSource<String> inputStream = env.readTextFile("D:\\code\\FlinkTutorial_1.10_New\\src\\main\\resources\\sensor.txt");
 //        DataStreamSource<String> inputStream = env.addSource(new FlinkKafkaConsumer011<String>("sensor", new SimpleStringSchema(), properties));
+        DataStreamSource<SensorReading> inputStream = env.addSource(new FlinkKafkaConsumer011<SensorReading>("sensor", new DeserializationSchema<SensorReading>() {
+            @Override
+            public SensorReading deserialize(byte[] bytes) throws IOException {
+                String value = new String(bytes, Charset.forName("UTF-8"));
+                String[] split = value.split(",");
+                return new SensorReading(split[0].trim(), Long.parseLong(split[1].trim()), Double.parseDouble(split[2].trim()));
+            }
+
+            @Override
+            public boolean isEndOfStream(SensorReading sensorReading) {
+                return false;
+            }
+
+            @Override
+            public TypeInformation<SensorReading> getProducedType() {
+                return TypeInformation.of(SensorReading.class);
+            }
+        }, properties));
         // 1. 基本转换操作：map成样例类类型
-        SingleOutputStreamOperator<String> dataStream = inputStream.map((MapFunction<String, String>) value -> {
+        SingleOutputStreamOperator<String> dataStream = inputStream.map((MapFunction<SensorReading, String>) value -> {
+            return value.toString();
+        });
+        /*SingleOutputStreamOperator<String> dataStream = inputStream.map((MapFunction<String, String>) value -> {
             String[] split = value.split(",");
             return new SensorReading(split[0].trim(), Long.parseLong(split[1].trim()), Double.parseDouble(split[2].trim())).toString();
-        });
+        });*/
 
         // 直接写入文件
-//        dataStream.writeAsText("D:\\code\\FlinkTutorial_1.10\\src\\main\\resources\\out");
+        dataStream.writeAsText("D:\\code\\FlinkTutorial_1.10_New\\src\\main\\resources\\out");
 
-        dataStream.addSink( StreamingFileSink.forRowFormat(
-//                new Path("D:\\code\\FlinkTutorial_1.10\\src\\main\\resources\\out1"),
-                new Path("hdfs://node101:9000/flink/hdfsSink"),
-                new SimpleStringEncoder<String>("UTF-8")
-        ).build());
+//        dataStream.addSink( StreamingFileSink.forRowFormat(
+////                new Path("D:\\code\\FlinkTutorial_1.10\\src\\main\\resources\\out1"),
+//                new Path("hdfs://node101:9000/flink/hdfsSink"),
+//                new SimpleStringEncoder<String>("UTF-8")
+//        ).build());
         /*dataStream.addSink(new FlinkKafkaProducer011<String>("node102:9092","test",new SimpleStringSchema()))
         .name("kafka sink");*/
         env.execute("test kafka source and sink job");
