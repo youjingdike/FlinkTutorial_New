ef0810c4506acb5c7f49eba924d26becffa7201f
dike <82329839@QQ.com>
2020-11-16 14:18:06 +0800

    add

--- src/main/java/com/xq/apitest/sinktest/FileSinkTest.java
+++ src/main/java/com/xq/apitest/sinktest/FileSinkTest.java
@@ -4,6 +4,7 @@ import com.xq.apitest.pojo.SensorReading;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.serialization.SimpleStringEncoder;
 import org.apache.flink.core.fs.Path;
+import org.apache.flink.formats.parquet.avro.ParquetAvroWriters;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -11,6 +12,7 @@ import org.apache.flink.streaming.api.functions.sink.filesystem.StreamingFileSin
 import org.apache.flink.streaming.api.functions.sink.filesystem.bucketassigners.BasePathBucketAssigner;
 import org.apache.flink.streaming.api.functions.sink.filesystem.bucketassigners.DateTimeBucketAssigner;
 import org.apache.flink.streaming.api.functions.sink.filesystem.rollingpolicies.DefaultRollingPolicy;
+import org.apache.flink.streaming.api.functions.sink.filesystem.rollingpolicies.OnCheckpointRollingPolicy;
 
 import java.net.URL;
 
@@ -32,30 +34,38 @@ public class FileSinkTest {
         dataStream.print();
 
         StreamingFileSink<SensorReading> sink1 = StreamingFileSink.forRowFormat(
-                new Path("D:\\code\\FlinkTutorial_1.10_New\\src\\main\\resources\\out_1.txt"),
+                new Path("D:\\code\\FlinkTutorial_1.10_New\\src\\main\\resources\\out_1"),
                 new SimpleStringEncoder<SensorReading>("UTF-8")
         ).withBucketAssigner(new BasePathBucketAssigner<>())
          .withRollingPolicy(DefaultRollingPolicy.builder().build())
          .build();
 
         StreamingFileSink<SensorReading> sink2 = StreamingFileSink.forRowFormat(
-                new Path("D:\\code\\FlinkTutorial_1.10_New\\src\\main\\resources\\out_2.txt"),
+                new Path("D:\\code\\FlinkTutorial_1.10_New\\src\\main\\resources\\out_2"),
                 new SimpleStringEncoder<SensorReading>("UTF-8")
         ).withBucketAssigner(new DateTimeBucketAssigner<>())
          .withRollingPolicy(DefaultRollingPolicy.builder().build())
          .build();
 
         StreamingFileSink<SensorReading> sink3 = StreamingFileSink.forRowFormat(
-                new Path("D:\\code\\FlinkTutorial_1.10_New\\src\\main\\resources\\out_3.txt")
+                new Path("D:\\code\\FlinkTutorial_1.10_New\\src\\main\\resources\\out_3")
                 ,new SimpleStringEncoder<SensorReading>()
         ).build();
+
+        StreamingFileSink<SensorReading> sink4 = StreamingFileSink.forBulkFormat(
+                new Path("D:\\code\\FlinkTutorial_1.10_New\\src\\main\\resources\\out_4")
+                , ParquetAvroWriters.forReflectRecord(SensorReading.class)
+        ).withBucketAssigner(new DateTimeBucketAssigner<>())
+                .withRollingPolicy(OnCheckpointRollingPolicy.build())
+                .build();
+
 
         /**
          * 报错：The writeAsCsv() method can only be used on data streams of tuples.
          */
 //        map.writeAsCsv("D:\\code\\FlinkTutorial_1.10_New\\src\\main\\resources\\out_4.txt");
 
-//        map.addSink(sink2);
+        dataStream.addSink(sink4);
 
         env.execute("file sink test");
 
